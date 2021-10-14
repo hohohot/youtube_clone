ddcbb479da11f8a822490b84eb47673917ec5934
hohohot <gc0216@naver.com>
2021-10-15 01:44:57 +0900

    add frontend

---
+++ backend/mytube/src/main/java/com/kc/portfolio/mytube/MytubeApplication.java
@@ -0,0 +1,13 @@
+package com.kc.portfolio.mytube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MytubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MytubeApplication.class, args);
+	}
+
+}

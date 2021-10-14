eb38918f7f3695ba895346fb69c069d2446a1c5b
hohohot <gc0216@naver.com>
2021-10-15 03:41:14 +0900

    vue springboot 연동

---
+++ backend/mytube/src/main/java/com/kc/portfolio/mytube/WebController.java
@@ -0,0 +1,19 @@
+package com.kc.portfolio.mytube;
+
+import lombok.Getter;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Getter
+@Controller
+public class WebController {
+
+    @GetMapping("/hello")
+    public String mainPage(){
+        System.out.println("hello");
+        return "index";
+    }
+
+
+
+}
